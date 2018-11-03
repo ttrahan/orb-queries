8f5b56f68d7ca85485bf886c57c66998e83723a1
Tom Trahan <tomt@circleci.com>
2018-11-03 14:05:47 -0700

    initial working version of partner-orbs query

--- query-partner-orbs-prod.js
+++ query-partner-orbs-prod.js
@@ -1,83 +1,113 @@
-var fs = require('fs')
-var dataByLine;
+var fs = require('fs');
+var util = require('util');
+const { exec } = require('child_process');
 
-function initialize() {
-	const dataFile = './data/prod-orbs.txt'
-    // Return new promise 
+function queryOrbs() {
+	// query production orbs from CircleCI registry
+    return new Promise(function(resolve, reject) {
+    	// Do async job
+		exec('circleci orb list -u > data/partner-orbs/prod-orbs.txt', function(err) {
+			if (err) {
+				reject(err);
+			} else {
+				console.log("Production orbs queried from registry...") 
+				resolve();
+			}
+		});
+    })
+}
+
+function loadProdOrbs() {
+	// read and prepare list of orbs
+	const dataFile = './data/partner-orbs/prod-orbs.txt'
     return new Promise(function(resolve, reject) {
     	// Do async job
 		fs.readFile(dataFile, 'utf8', function(err, data) {
 			if (err) {
 				reject(err);
 			} else {
+				// split prodOrbs array into namespaces and orbs/versions
 				var dataByLine = data.split("\n")
-				resolve(dataByLine);
-			}
-		});
-    })
-}
-
-function loadPartners() {
-	const dataFile = './data/partners.txt'
-    // Return new promise 
-    return new Promise(function(resolve, reject) {
-    	// Do async job
-		fs.readFile(dataFile, 'utf8', function(err, data) {
-			if (err) {
-				reject(err);
-			} else {
-				var dataByLine = data.split("\n")
-				resolve(dataByLine);
-			}
-		});
-    })
-}
-
-function main() {
-    var initializePromise = initialize();
-    initializePromise.then(function(result) {
-        var dataByLine = result;
-        // use the list of orbs
-		console.log('Production orbs read from file...');
-		var prodOrbs = dataByLine.slice(2,-1); 
-		var prodOrbsSplit = [];
-		prodOrbs.forEach(function(orb) {
-			prodOrbsSplit.push(orb.split("/"));
-		})
-		return prodOrbsSplit;
-    }, function(err) {
-        console.log(err);
-	}).then(function(result) {
-		var prodOrbsSplit = result;
-        var loadPartnersPromise = loadPartners();
-		loadPartnersPromise.then(function(result) {
-		    console.log('Partners read from file...');
-			// convert partners list to lowercase and sort
-			const partners = [];
-			result.slice(0,-1).forEach(function(item) {
-				partners.push(item.toLowerCase())
-			});
-			partners.sort();
-			var partnerOrbsSplit = [];
-			prodOrbsSplit.forEach(function(orb) {
-				partners.forEach(function(partner) {
-					if (orb[0].includes(partner) || (orb[1].includes(partner) && orb[0] == 'circleci')) {
-						partnerOrbsSplit.push(orb);
-					};
+				var prodOrbs = dataByLine.slice(2,-1); 
+				var prodOrbsSplit = [];
+				prodOrbs.forEach(function(orb) {
+					prodOrbsSplit.push(orb.split("/"));
 				})
-			});
-			var partnerOrbs = [];
-			partnerOrbsSplit.forEach(function(orb) {
-				partnerOrbs.push(orb[0] + "/" + orb[1])
-			})
-			console.log(partnerOrbs);
-			fs.writeFile('partner-orbs.csv', partnerOrbs.sort(), err => {
-				if (err) throw err;
-			});
-		}), function(err) {
-			console.log(err);
-		}
-	})
-};
+				console.log('Production orbs read from file...');
+				resolve(prodOrbsSplit);
+			}
+		});
+    })
+}
 
-main();
+function loadPartners(result) {
+	// load and prep list of partners >>filter production orbs by partners
+	const prodOrbsSplit = result;	
+	const dataFile = './data/partner-orbs/partners.txt'
+    // Return new promise 
+    return new Promise(function(resolve, reject) {
+    	// Do async job
+		fs.readFile(dataFile, 'utf8', function(err, data) {
+			if (err) {
+				reject(err);
+			} else {
+				console.log('Partners read from file...');
+				// convert partners list to lowercase, remove spaces, and sort
+				var dataByLine = data.split("\n")
+				var partners = [];
+				dataByLine.slice(0,-1).forEach(function(item) {
+					partners.push(item.toLowerCase().replace(/ /g,''))
+				});
+				partners.sort();
+				console.log('\nPartners:');
+				console.log(partners);
+				// filter productionOrbs for partner-related orbs (produced by 
+				// either CCI or partner)
+				var partnerOrbsSplit = [];
+				prodOrbsSplit.forEach(function(orb) {
+					partners.forEach(function(partner) {
+						if (orb[0].includes(partner) || (orb[1].includes(partner) 
+							&& orb[0] == 'circleci')) {
+							partnerOrbsSplit.push(orb);
+						}
+					});
+				});
+				resolve(partnerOrbsSplit);
+			}
+    	});
+	});
+}
+
+function filterPartnerOrbs(result) {
+    return new Promise(function(resolve, reject) {
+		// filter productionOrbs for partner-related orbs, produced by 
+		// either CCI or partner
+		const partnerOrbsSplit = result;
+		var partnerOrbs = [];
+		partnerOrbsSplit.forEach(function(orb) {
+			partnerOrbs.push(orb[0] + "/" + orb[1])
+		});
+		resolve(partnerOrbs);
+	});
+}
+
+function outputPartnerOrbs(result) {
+	// output
+	partnerOrbs = result;	
+	console.log('\nPartner-related orbs...');
+	console.log(partnerOrbs);
+	fs.writeFile('data/partner-orbs/partner-orbs.csv', partnerOrbs.sort(), err => {
+		if (err) throw err;
+	});
+}
+
+var fnlist = [ queryOrbs, loadProdOrbs, loadPartners, filterPartnerOrbs, outputPartnerOrbs ];
+
+function main(list) {
+	var p = Promise.resolve();
+	return list.reduce(function(pacc, fn) {
+		return pacc = pacc.then(fn);
+	}, p);
+}
+
+main(fnlist);
